5c4d6dcfdb647ecc73c5bc61fe858f327cd198f3
Joe Wright <joe.wright@jestercom.com>
2013-12-31 11:03:52 -0500

    init ui namespace

--- index.js
+++ index.js
@@ -7,3 +7,4 @@ Ti.Filesystem = require('./lib/filesystem');
 Ti.Geolocation = require('./lib/geolocation');
 Ti.Network = require('./lib/network');
 Ti.XML = require('./lib/xml');
+Ti.UI = require('./lib/ui');
